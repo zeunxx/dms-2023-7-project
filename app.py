bbc6ad95e6b98a6330c6ddc9b3e5563f555f2202
zeun <real_jieun99@daum.net>
2023-05-05 19:39:46 +0900

    ui 서버에서 메시지 받아와 producer에 전달

--- app.py
+++ app.py
@@ -2,9 +2,12 @@ from flask import Flask
 from flask import *
 from kafka import KafkaProducer
 from json import dumps
+import sys
 import time
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
 def producerSend(writer, timestamp, content):
     producer = KafkaProducer(bootstrap_servers='localhost:9092', value_serializer=lambda x: dumps(x).encode('utf-8'))
@@ -15,9 +18,10 @@ def producerSend(writer, timestamp, content):
 
 @app.route("/")
 def index():
+    print("홈 들어옴")
     return "home"
-
-@app.route("/msg-send",methods=['POST'])
+    
+@app.route("/msg_send",methods=['POST'])
 def producer_test():
     params = request.get_json()
     writer = params['writer']
@@ -25,7 +29,5 @@ def producer_test():
     content = params['content']
     producerSend(writer, timestamp, content)
     return "ok"
-    
-    
 
-app.run(port=5000, debug=True)
+app.run(port=8989, debug=True)
